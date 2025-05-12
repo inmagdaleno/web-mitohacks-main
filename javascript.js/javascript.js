57b80996f6b65a10d52f72ae28bd0a3ce7cec7b8
inmagdaleno <inmagdaleno@gmail.com>
2025-05-12 09:27:56 +0200

    ultima

---
+++ javascript.js/javascript.js
@@ -0,0 +1,9 @@
+// declarar variables
+let nombreAlumno = "Manolo"
+let apellidoAlumno = "Pérez"
+let edad = 28;
+let calificacion = 8
+
+alert("Hola" + " " + nombreAlumno + " " + apellidoAlumno, "has sacado un" + " " + calificacion + " " + "este trimestre");
+
+console.log("Hola desde consola");
